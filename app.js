a50401e0cd307013278847f96876950d28549a56
DBEST-EZRA <ezradbest101@gmail.com>
2025-02-03 01:03:40 +0300

    connected to mongodb

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // Connection to mongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // User Schema
 const UserSchema = new mongoose.Schema({
